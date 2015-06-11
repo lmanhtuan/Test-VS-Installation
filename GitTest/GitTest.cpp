f91e5c3f325a122a20230b097485a043cc328b84
lmanhtuan <tlemanh@pentalog.fr>
2015-06-11 17:13:15 +0700

    Init version

---
+++ GitTest/GitTest.cpp
@@ -0,0 +1,16 @@
+// GitTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello world";
+	int i;
+	cin >>i;
+	return 0;
+}
+
